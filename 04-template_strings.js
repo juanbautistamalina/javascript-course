1581e7982e6b2f5da751c7b9769039c367b89b1c
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-02 19:01:33 -0300

    Template Strings

---
+++ 04-template_strings.js
@@ -0,0 +1,10 @@
+let nombre = "Juan Bautista";
+let apellido = "Malina";
+
+// Concatenación
+let saludo = "Hola, mi nombre es " + nombre + " " + apellido;
+console.log(saludo);
+
+// Interpolación (template string)
+let saludo2 = `Hola, mi nombre es ${nombre} ${apellido}!`;
+console.log(saludo2);
