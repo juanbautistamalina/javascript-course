21e15e36a007919e7661f3f9347d237f855e90b7
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-02 19:00:42 -0300

    Variables

---
+++ 01-variables.js
@@ -0,0 +1,27 @@
+
+
+// Diferencia entre var y let
+
+console.log("*** VAR ***");
+var a = "Valor Inicial";
+console.log(a);
+
+{
+    var a = "Valor Modificado";
+    console.log(a);
+}
+
+console.log(a);
+
+
+
+console.log("*** LET ***");
+let b = "Valor Inicial";
+console.log(b);
+
+{
+    let b = "Valor Modificado";
+    console.log(b);
+}
+
+console.log(b);
