8e7b7dba222529912a33d88ab1b302aa11a65993
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-02 19:02:48 -0300

    Undefined, Null y NaN

---
+++ 07-undefined-null-nan.js
@@ -0,0 +1,9 @@
+// undefined: Significa que la variable no tiene contenido.
+let indefinida;
+
+// null: Significa que la variable no tiene contenido, pero en este caso el programador asigna null intencionalmente
+let nulo = null;
+
+// NaN (Not a Number): Ocurre cuando se realizan operaciones con tipos de datos no numéricos
+let noEsUnNumero = "hola" * 12;
+console.log(noEsUnNumero);
