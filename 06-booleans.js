5d361ed3247477f095e83987dd768b5765acbb61
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-02 19:02:18 -0300

    Booleans

---
+++ 06-booleans.js
@@ -0,0 +1,26 @@
+let verdadero = true;
+let falso = false;
+
+// true
+// if (true)
+// if ({})
+// if ([])
+// if (42)
+// if ("0")
+// if ("false")
+// if (new Date())
+// if (-42)
+// if (12n)
+// if (3.14)
+// if (-3.14)
+// if (Infinity)
+// if (-Infinity)
+
+// false
+// if (false)
+//   if (null) 
+//   if (undefined)
+//   if (0)
+//   if (-0)
+//   if (NaN)
+//   if ("")
