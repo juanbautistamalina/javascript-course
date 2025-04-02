6bcd49defbf3f5237b5254d24a4a9654585f72bd
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-02 19:01:16 -0300

    Strings

---
+++ 03-strings.js
@@ -0,0 +1,8 @@
+let cadena = "Juan Bautista Malina";
+let cadena2 = "Cadena 2";
+let cadena3 = new String("Cadena 3");
+
+console.log(cadena, cadena2, cadena3);
+
+console.log("Cadena: ", cadena);
+console.log("Longitud: ", cadena.length);
