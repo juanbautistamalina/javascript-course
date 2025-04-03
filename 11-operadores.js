e88b208d2d0babdf9968eb965169032d01fc8cfb
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-03 20:46:31 -0300

    Operadores

---
+++ 11-operadores.js
@@ -0,0 +1,22 @@
+// Operadores
+
+// - Aritméticos: + - * / % ()
+let a = 50;
+let b = 25;
+let c = 100 + (a + b) * 2;
+console.log(c);
+
+// - Relacionales: >, <, <=, >=, ==, ===, !=, !==                                                          
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(10 == "10") // true
+console.log(10 === "10"); // false
+
+// - Incremento y Decremento: +=, -=, *=, /=, %=
+// - Unario: ++, --
+let i = 1;
+console.log(i++) // Primero muestra por consola el valor de i, y luego incrementa su valor
+i = 1;
+console.log(++i); // Primero incrementa el valor de i en 1 y luego lo muestra por consola
+
+// - Lógicos: ! Not, || Or, && And
