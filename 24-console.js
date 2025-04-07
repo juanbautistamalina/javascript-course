35c6c96688b7c9a1afdcb2e5b8273448e98f4a5b
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-07 19:44:28 -0300

    Objeto console

---
+++ 24-console.js
@@ -0,0 +1,42 @@
+console.log(console); 
+console.error("Mensaje de Error");
+console.warn("Mensaje de Advertencia");
+console.info("Mensaje Informativo");
+console.log("Mensaje para registrar lo que ocurre en la aplicación");
+
+// Limpiar la consola
+// console.clear()
+
+// Crear grupos en la consola
+console.group("🎨 Colores principales");
+console.log("Rojo: #FF0000");
+console.log("Verde: #00FF00");
+console.log("Azul: #0000FF");
+console.groupEnd(); 
+
+// Crear una tabla en la consola
+console.table(Object.entries(console));
+
+const producto = {
+  nombre: "Teclado",
+  precio: 45,
+  disponible: true,
+};
+
+console.table(producto);
+
+console.time("Cuánto tiempo tarda mi código");
+const arreglo = Array(1000000);
+
+for (let i = 0; i < arreglo.length; i++) {
+    arreglo[i] = i;
+}
+
+console.timeEnd("Cuánto tiempo tarda mi código");
+
+
+// Contar la cantidad de veces que se ejecuta un bloque de código
+for (let i = 0; i <= 100; i++) {
+    console.count("contador del for");
+    console.log(i);
+}
