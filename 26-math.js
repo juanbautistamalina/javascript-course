941f93b145aeee05ec3a6097554610f8eca1e0a9
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-08 15:23:36 -0300

    Objeto Math

---
+++ 26-math.js
@@ -0,0 +1,13 @@
+console.log(Math.PI); // Valor de PI
+console.log(Math.abs(-10)); // Valor absoluto: convierte un número negativo en positivo.
+console.log(Math.ceil(5.1)); // Redondea hacia arriba al entero más cercano.
+console.log(Math.floor(5.1)); // Redondea hacia abajo al entero más cercano.
+console.log(Math.round(5.5)); // Redondea al número entero más cercano
+console.log(Math.sqrt(81)); // Raíz cuadrada
+console.log(Math.pow(2, 4)); // Potencia -> Math.pow(base, exponente)
+
+// Si el número es negativo, devuelve -1. Si el número es 0, devuelve 0. Si el número es positivo, devuelve 1
+console.log(Math.sign(86));
+
+console.log(Math.random()); // Devuelve un número aleatorio entre 0 y 1 (sin incluir el 1)
+console.log(Math.floor(Math.random() * 1000 + 1)) // Gererar un número aleatorio entre 0 y 1000 (incluyendo el 1000)
