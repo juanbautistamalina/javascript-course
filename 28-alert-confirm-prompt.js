b38518e5bb60d0cee4d871aff6dd891f24517e8b
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-09 13:42:02 -0300

    alert, confirm y prompt

---
+++ 28-alert-confirm-prompt.js
@@ -0,0 +1,8 @@
+// Ejecutar en el Navegador, ya que Node.js no posee el objeto Window
+
+alert("Mensaje de Alerta");
+const confirmacion = confirm("¿Aceptas los términos y condiciones?");
+const input = prompt("Ingresa texto");
+
+console.log(confirmacion);
+console.log(input);
