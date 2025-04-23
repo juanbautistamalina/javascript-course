2a97421777f6701ed43ce741f4dc50ddd185e6fb
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-04-23 12:16:26 -0300

    Ejercicios de Async y Await

---
+++ 36-async-await-practica/async-await-practica-1.js
@@ -0,0 +1,32 @@
+const usuarios = [
+  { id: 1, nombre: "Ana" },
+  { id: 2, nombre: "Luis" },
+  { id: 3, nombre: "Carlos" },
+];
+
+function obtenerUsuario(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const usuario = usuarios.find(u => u.id === id);
+      
+      if (usuario)
+        resolve(usuario);
+      
+      return reject("Usuario no encontrado");
+
+    }, 1000);
+  });
+}
+
+// TODO: Usar async/await para llamar a obtenerUsuario(2) y mostrar el resultado.
+async function funcionAsincrona() {
+  try {
+    const id = 2;
+    let usuario = await obtenerUsuario(id);
+    console.log(usuario);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+funcionAsincrona();
